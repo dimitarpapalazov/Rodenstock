9603c0947da8372c515d106c8136751c751fbd48
Angela <angela.sotirovska@students.finki.ukim.mk>
2020-09-10 13:33:16 +0200

    modelite

---
+++ Rodenstock/Models/Glasses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rodenstock.Models
+{
+    public class Glasses
+    {
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public string Material { get; set; }
+        public string Sex { get; set; }
+        public int Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ Rodenstock/Models/Orders.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rodenstock.Models
+{
+    public class Orders
+    {
+        public Client client { get; set; }
+        public List<Glasses> glasses { get; set; }
+        public Orders()
+        {
+            glasses = new List<Glasses>();
+        }
+    }
+}
